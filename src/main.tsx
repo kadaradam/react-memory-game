b5a9835c5458edb8d3ab99140b79c6f5868a87a0
kadaradam <xadam1996@gmail.com>
2023-04-12 10:46:38 +0200

    feat: add Mui theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { ThemeStateProvider } from './theme';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<ThemeStateProvider>
+			<App />
+		</ThemeStateProvider>
+	</React.StrictMode>,
+);
